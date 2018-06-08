4c42589a74ad80875873df847346116ff00fe869
NpF <natdanai.prs@gmail.com>
2018-06-08 17:26:47 +0700

    create file for template 3

---
+++ pdf/type_3.php
@@ -0,0 +1,20 @@
+<?php
+	require_once '../dompdf/autoload.inc.php';
+
+	use Dompdf\Dompdf;
+
+	// instantiate and use the dompdf class
+	$dompdf = new Dompdf();
+	$fileContent = file_get_contents( "http://localhost/pdf_dot/template_3.html" ) ;
+	$dompdf->loadHtml($fileContent);
+
+	// // (Optional) Setup the paper size and orientation
+	$dompdf->setPaper('A4', 'portrait');
+
+	// Render the HTML as PDF
+	$dompdf->render();
+
+	// Output the generated PDF to Browser
+	$dompdf->stream("type_3.pdf", array("Attachment"=>0));
+
+?>
